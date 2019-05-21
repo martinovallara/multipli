fbec0cd4bbeac7c271022428a94e15cdb593ea09
Martino Vallara <martino.vallara@xpeppers.com>
2019-05-21 17:45:55 +0200

    add class counter

--- index.js
+++ index.js
@@ -4,18 +4,34 @@ const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
+
+
 var numeroDiInteriDaEsaminare = 0;
 var recursion = 0;
+
+var MultiploCounter = class {
+
+    constructor(multiplo) {
+        this.multiplo = multiplo;
+        this.counter=0;
+    }
+    IncrementaSeMultiplo(numero) {
+        if (numero % this.multiplo == 0) this.counter = this.counter + 1;
+    }
+};
+
 rl.question('Inserisci quanti numeri interi vuoi esaminare? ', (input) => {
 
     console.log(`Esaminero ${input} numeri interi`);
 
     numeroDiInteriDaEsaminare = input;
-
     response = {
-        multiplidiDue: 0,
+        multipliDiDue: new MultiploCounter(2),
         multipliDiCinque: 0,
         multipliDiDieci: 0,
+        IncrementaSeMultiploDiDue: function (numero) {
+            if (numero % 2 == 0) this.multipliDiDue = this.multipliDiDue + 1;
+        }
     };
 
     LeggiINumeriDaEsaminare = () => {
@@ -24,8 +40,8 @@ rl.question('Inserisci quanti numeri interi vuoi esaminare? ', (input) => {
             numeroDiInteriDaEsaminare = numeroDiInteriDaEsaminare - 1;
 
             console.log(`Hai inserito il numero: ${numero} `);
-            if (numero % 2 == 0) response.multiplidiDue = response.multiplidiDue + 1;
- 
+            response.multipliDiDue.IncrementaSeMultiplo(numero);
+
             if (numeroDiInteriDaEsaminare == 0) {
                 console.log(response);
                 return rl.close();
